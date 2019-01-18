4c510a8a16f715e4b2c458ccefef549fea2b88ca
Arijit Dasgupta <arijit.dasgupta@smaato.com>
2019-01-18 15:07:24 +0100

    Changed react-dom invocation

--- src/TestCaseFactory.js
+++ src/TestCaseFactory.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
+import TestUtils from 'react-dom/test-utils';
 import Sizzle from 'sizzle';
 
 export class TestCase {
